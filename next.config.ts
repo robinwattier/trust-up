f723b468b7be90d47430446739c64ba936182c24
Robin Wattier <rwcnx10n@gmail.com>
2026-04-15 10:50:44 +0200

    url

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/trust-up',
+  assetPrefix: '/trust-up',
   images: {
     unoptimized: true,
   },
